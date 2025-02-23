49f469b35ae472cc3aced07f531ad220be7941ca
肖旭龙 <xiaoxulong@aa.com>
2025-02-23 17:23:22 +0800

    local

--- src/views/Share.vue
+++ src/views/Share.vue
@@ -1,85 +1,197 @@
 <template>
-  <div class="root">
+  <div
+    class="root"
+  
+  >
+  <!-- v-infinite-scroll="addShowPic"
+    :infinite-scroll-disabled="noMorePic"
+    infinite-scroll-distance="200" -->
     <pageTop></pageTop>
-    <article>
-      <h2>网友投稿</h2>
-      <h5>SHARE</h5>
-    </article>
+    <van-dropdown-menu >
+      <van-icon name="search" />
+      <van-dropdown-item v-model="value1" :options="option1" @change="change"/>
+</van-dropdown-menu>
+    <img class="title_img" src="../assets/wallPicBackGround.png" />
     <div v-for="item in showPic" :key="item" class="itemContain">
-      <img class="backImg" :src="item.url" @click="goDownload(item.url)">
+      <img class="backImg" :src="item.url" @click="goDownload(item)" />
       <center class="imgName">{{ item.name }}</center>
     </div>
-    <button v-show="!noMorePic" @click="addShowPic">更多无水印壁纸</button>
-    <button style="color: orange" v-show="noMorePic">无更多壁纸了！</button>  </div>
+    <!-- <button v-show="!noMorePic" @click="addShowPic">更多无水印壁纸</button>
+    <button style="color: orange" v-show="noMorePic">无更多壁纸了！</button> -->
+    <van-pagination @change="pageChange" v-model="currentPage" :total-items="totalNum" :items-per-page="10" />
+  </div>
 </template>
 
 <script>
-import pageTop from '../components/pageTop'
-// import { sharePics } from '../../http/http'
-
-import { onMounted, ref, getCurrentInstance } from 'vue'
+import { examplePics, allPics, shootreq } from "../../http/http";
+import pageTop from "../components/pageTop";
+import { onMounted, ref, getCurrentInstance } from "vue";
+import { useRouter } from "vue-router";
+import { DropdownMenu, DropdownItem ,Pagination } from 'vant';
 export default {
-  name: 'Share',
+  name: "WallPaper",
   components: {
-    pageTop
+    pageTop,
   },
   setup() {
-    // let { proxy } = getCurrentInstance()
-    // onMounted(() => {
-    //   document.documentElement.scrollTo(0, 0);
-    //   const top = document.documentElement.scrollTop || document.body.scrollTop;
-    // })
-    // let showPic = ref([])
-    // let showPicNum = ref(5)
-    // let noMorePic = ref(false)
-    // if (sharePics.length > 5) {
-    //   showPic.value = sharePics.slice(0, showPicNum.value);
-    // } else {
-    //   showPic.value = sharePics
-    // }
-    // let addShowPic = function () {
-    //   showPicNum.value += 5
-    //   if (sharePics[showPicNum.value - 1] != undefined) {
-    //     showPic.value = sharePics.slice(0, showPicNum.value);
-    //   } else {
-    //     showPic.value = sharePics
-    //     noMorePic.value = true
-    //   }
-    // }
-    // let goDownload = function (url) {
-    //   proxy.$router.push({
-    //     path: '/PicDownload', query: {
-    //       url: url
-    //     }
-    //   })
-    // }
-    // return { sharePics, showPic, showPicNum, addShowPic, noMorePic, goDownload }
-  }
+    console.log(allPics);
+    const value1 = ref('默认排序');
+    const currentPage = ref(1);
+    const option1 = [
+      { text: '默认排序', value: '默认排序' },
+      { text: '彦', value: '彦' },
+      { text: '阿追', value: '阿追' },
+      { text: '蔷薇', value: '蔷薇' },
+      { text: '鹤熙', value: '鹤熙' },
+      { text: '凯莎', value: '凯莎' },
+      { text: '凉冰', value: '凉冰' },
+      { text: '琪琳', value: '琪琳' },
+      { text: '炙心', value: '炙心' },
+      { text: '阿狸', value: '阿狸' },
+      { text: '蕾娜', value: '蕾娜' },
+      { text: '怜风', value: '怜风' },
+      { text: '舞照', value: '舞照' },
+      { text: '群像', value: '群像' },
+    ];
+    let show = ref(true);
+    const wid = ref({ imgHei: 0 });
+    wid.value.imgHei = document.documentElement.clientWidth * 0.8 + "px";
+    console.log(wid.value.imgHei);
+    let { proxy } = getCurrentInstance();
+    onMounted(() => {
+      totalNum.value=allPics.length
+ document.documentElement.scrollTo(0, 0);
+      const top = document.documentElement.scrollTop || document.body.scrollTop;
+    });
+    let showPic = ref([]);
+    let showPicNum = ref(10);
+    let totalNum = ref(0);
+    let noMorePic = ref(false);
+    let newPic=ref([])
+    showPic.value = allPics.slice(0, showPicNum.value);
+
+    let pageChange = function (num) {
+   
+      currentPage.value=num
+      console.log(num)
+      if (value1.value==='默认排序') {
+        showPic.value = allPics.slice((num-1)*10, num*10);
+        console.log(showPic.value )
+      }else{
+        showPic.value = newPic.value.slice((num-1)*10, num*10)
+        console.log(showPic.value )
+      }
+      document.documentElement.scrollTo(0, 0);
+      const top = document.documentElement.scrollTop || document.body.scrollTop;
+
+    };
+    
+    let addShowPic = function () {
+
+      showPicNum.value += 5;
+      if (value1.value==='默认排序') {
+        if (allPics[showPicNum.value - 1] != undefined) {
+        showPic.value = allPics.slice(0, showPicNum.value);
+      } else {
+        showPic.value = allPics;
+        noMorePic.value = true;
+      }
+      }else{
+        if (newPic.value[showPicNum.value - 1] != undefined) {
+        showPic.value = newPic.value.slice(0, showPicNum.value);
+      } else {
+        showPic.value = newPic.value;
+        noMorePic.value = true;
+      }
+      }
+
+    };
+    let goDownload = function (data) {
+      console.log(data);
+      proxy.$router.push({
+        path: "/PicDownload",
+        query: {
+          data: JSON.stringify(data),
+        },
+      });
+    };
+    let  change= function (data) {
+if (data!=='默认排序') {
+ 
+  newPic.value=[]
+  currentPage.value=1
+  allPics.forEach((item)=>{
+if(item.name.indexOf(data) !==-1){
+newPic.value.push(item)
 }
+})
+showPic.value =newPic.value.slice(0, 10);
+totalNum.value=newPic.value.length
+document.documentElement.scrollTo(0, 0);
+const top = document.documentElement.scrollTop || document.body.scrollTop;
+}else{
+  location.reload()
+}
+
+console.log(newPic.value);
+    }
+    return {
+      newPic,
+      value1,
+      option1,
+      show,
+      allPics,
+      showPic,
+      showPicNum,
+      addShowPic,
+      noMorePic,
+      goDownload,
+      wid,
+      change
+    ,totalNum,
+     currentPage,
+     pageChange
+    };
+  },
+};
 </script>
 
 <style scoped lang="less">
+:deep(.van-cell:first-child){
+  margin-top:0;
+}
+.van-dropdown-menu{
+width: auto;
+position: absolute;
+top: 65px;
+right: 120px;
+z-index: 99;
+text-align: center;
+line-height: 50px;
+color: white;
+}
+:deep(.van-dropdown-menu__bar){
+  background: none;
+  color: red !important;
+  position: absolute;
+  z-index: 999;
+  width: 50px;
+height: 65px;
+}
+:deep(.van-ellipsis){
+  color: red !important;
+  font-size: 0px;
+  height: 50px;
+  text-align: center;
+  line-height: 50px;
+}
+:deep(.van-dropdown-menu__title):after{
+  content:"";
+  border:none
+}
 .root {
-  background-color: #10171a;
   width: 100%;
   height: auto;
-}
-
-article {
-  margin-bottom: 20px;
-}
-
-h2 {
-  font-size: 35px;
-  text-align: center;
-  color: white;
-}
-
-h5 {
-  margin-top: 15px;
-  font-size: 20px;
-  text-align: center;
-  color: white;
 }
 
 .title_img {
@@ -91,22 +203,17 @@ h5 {
   transform: translateX(-50%);
 }
 
-
-
 .itemContain {
   margin-bottom: 20px;
 
   .backImg {
     width: 94%;
     margin-left: 3%;
-    height: 328Px;
+    height: v-bind("wid.imgHei");
     object-fit: cover;
     object-position: 50% 15%;
     border: 1px solid #01e7ff;
   }
-
-
-
 
   .imgName {
     color: white;
@@ -114,19 +221,20 @@ h5 {
   }
 }
 
-
-
 button {
   font-size: 26px;
   width: 90%;
   height: 70px;
   margin-left: 5%;
-  background: rgba(9, 61, 118, .3);
-  border: solid 1px #40D7F7;
+  background: rgba(9, 61, 118, 0.3);
+  border: solid 1px #40d7f7;
   color: white;
   text-align: center;
   line-height: 70px;
-  margin-top: 10Px;
+  margin-top: 10px;
   margin-bottom: 10px;
+}
+.van-icon-search{
+  font-size: 50px;
 }
 </style>
