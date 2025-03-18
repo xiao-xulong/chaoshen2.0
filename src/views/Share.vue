c85bb3998a0163601f0bbb5083dd3f7054e2f6ff
肖旭龙 <xiaoxulong@aa.com>
2025-03-18 19:23:07 +0800

    local\

--- src/views/Share.vue
+++ src/views/Share.vue
@@ -97,9 +97,9 @@ num=num+Number(i.money)
       { text: '韩月', value: '韩月' },
       { text: '曹颖', value: '曹颖' },
       { text: '比比东', value: '比比东' },
-      { text: '剑妈', value: '剑妈' },
-      { text: '云曦', value: '云曦' },
-      { text: '小舞', value: '小舞' },
+      // { text: '剑妈', value: '剑妈' },
+      // { text: '云曦', value: '云曦' },
+      // { text: '小舞', value: '小舞' },
     ];
     let show = ref(true);
     const wid = ref({ imgHei: 0 });
